d4ed4c04d72c284cec326e0d6ffcf92ee1d1ea2d
goldsteinmarcmd <mgolds9@gmail.com>
2018-01-31 17:48:05 -0800

    Add files via upload

---
+++ HTML_HW/Untitled-1.js
@@ -0,0 +1,13 @@
+var x = 1;
+var y = 10;
+
+if (y > x) {
+    console.log("you crazy");
+}
+
+
+if (x === 1){
+    if y > 20
+        console.log("ugh heath farted");
+}   else (y + x < 20){
+        console.log("nvm it wasnt heath who farted. it was someone else. maybe it was me?");}
